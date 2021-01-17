4acd5a0bef2860be4617361bd8eb4c7ae5345a24
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-17 12:52:39 +0000

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,13 +1,8 @@
 <?php
-
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-
-$db_name = "test_db";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
-
-if (!$conn) {
-	echo "Connection failed!";
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, '117380531fyp.database.windows.net', 'fyp117380531', 'FYP2021!', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+?>
