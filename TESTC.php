0eefff2201d3e02b021583c8bf7efb3ed4b8b12f
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-17 13:49:13 +0000

    Update TESTC.php

--- TESTC.php
+++ TESTC.php
@@ -1,11 +1,3 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:117380531fyp.database.windows.net,1433; Database = FYP117380531", "fyp117380531", "FYP2021!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("You have Bear First?");
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    phpinfo();
+?>
